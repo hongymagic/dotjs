ffd90f36c4c84c03dfc9a8d43c215469801f3af0
hongymagic <d.hong@me.com>
2013-03-21 21:26:01 +1100

    Clean up some code and add random functions

--- didyouworktoday.com.js
+++ didyouworktoday.com.js
@@ -1,35 +1,54 @@
+//
+// DOM element detection
+
 var $calendar = $('.calendar');
 var $days     = $calendar.find('li');
 
-$('*').css('-webkit-backface-visibility', 'hidden');
+//
+// CSS styles to be applied initially (preferrably via .css files). This is
+// run once on startup.
 
-function initialize ($element, column) {
-	var offsetX = (-1 * column * 130) + 'px';
-
-	return $element.css({
-		'-webkit-transform'           : 'translate3d(0, 0, 0) perspective(2500px) rotate3d(0, 1, 0, -90deg)',
-		'-webkit-transform-origin-x'  : offsetX
+function initialize() {
+	$('*').css({
+		'-webkit-backface-visibility': 'hidden'
 	});
 }
 
-function apply ($element, column, row) {
-	setTimeout(function () {
-		$element.css({
-			'-webkit-transform'           : 'perspective(2500px) rotate3d(0, 1, 0, 0deg)',
-			'-webkit-transition'          : 'all 1s'
-		});
-	}, column * (row || 1) * 100);
+initialize();
+
+//
+// If calendar exists, let's animate it like Windows 8 style!
+
+function random (min, max) {
+    return Math.random() * (max - min) + min;
 }
 
-$calendar.css({
-	'overflow': 'visible'
-});
+function initialState ($element, column) {
+	var offsetX = (-1 * column * 130) + 'px';
+
+	return $element.css({
+		'-webkit-transform'          : 'translate3d(0, 0, 0) perspective(1800px) rotate3d(0, 1, 0, -90deg)',
+		'-webkit-transform-origin-x' : offsetX
+	});
+}
+
+function animationState ($element, column, row) {
+	var seed     = random (1, 5);
+	var duration = random (0.3, 1.5);
+	var delay    = seed * column * (random(20, 100) | 0);
+	setTimeout(function () {
+		$element.css({
+			'-webkit-transform'  : 'perspective(1800px) rotate3d(0, 1, 0, 0deg)',
+			'-webkit-transition' : 'all 1s'
+		});
+	}, delay);
+}
 
 if ($days) {
 	$days.each(function (index, element) {
 		var column = index % 7;
 		var row = parseInt(index / 7, 10);
-		var $element = initialize($(element), column);
-		apply($element, column, row);
+		var $element = initialState($(element), column);
+		animationState($element, column, row);
 	});
 }
