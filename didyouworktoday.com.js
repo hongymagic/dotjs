649ee5351d9d77cd7cc401fd39a3bbd47e834e25
hongymagic <d.hong@me.com>
2013-03-21 14:57:31 +1100

    Improve performance of transformation

--- didyouworktoday.com.js
+++ didyouworktoday.com.js
@@ -1,16 +1,24 @@
 var $calendar = $('.calendar');
 var $days     = $calendar.find('li');
 
+$('*').css('-webkit-backface-visibility', 'hidden');
+
 function initialize ($element, column) {
 	var offsetX = (-1 * column * 130) + 'px';
 
-	$element.css({
-		'-webkit-transform'           : 'perspective(1500px) rotateY(-90deg)',
-		'-webkit-transform-origin-x'  : offsetX,
-		'-webkit-transition'          : 'all ' + column + 's'
+	return $element.css({
+		'-webkit-transform'           : 'translate3d(0, 0, 0) perspective(2500px) rotate3d(0, 1, 0, -90deg)',
+		'-webkit-transform-origin-x'  : offsetX
 	});
+}
 
-	return $element;
+function apply ($element, column, row) {
+	setTimeout(function () {
+		$element.css({
+			'-webkit-transform'           : 'perspective(2500px) rotate3d(0, 1, 0, 0deg)',
+			'-webkit-transition'          : 'all 1s'
+		});
+	}, column * (row || 1) * 100);
 }
 
 $calendar.css({
@@ -22,5 +30,6 @@ if ($days) {
 		var column = index % 7;
 		var row = parseInt(index / 7, 10);
 		var $element = initialize($(element), column);
+		apply($element, column, row);
 	});
 }
